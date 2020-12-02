c0d9d876acd56cea51e97e5d894169b637b9e348
Yahya Fazlani <yahyafazlani@outlook.com>
2020-12-02 23:05:19 +0530

    first commit

--- qr_code_generator/urls.py
+++ qr_code_generator/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wifi-qr-code-generator/', include('WiFi_QR_Code_Generator.urls'))
 ]
