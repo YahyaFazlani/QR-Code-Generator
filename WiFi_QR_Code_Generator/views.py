f40b6e0c3d76d6bb90adfc1b003cb82cac6431b4
Yahya Fazlani <yahyafazlani@outlook.com>
2020-12-03 10:19:52 +0530

    Loaded static

--- WiFi_QR_Code_Generator/views.py
+++ WiFi_QR_Code_Generator/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
 def index(request):
   return render(request, "WiFi_QR_Code_Generator/index.html")
+
+class ResultView(TemplateView):
+  template_name = "WiFi_QR_Code_Generator/result.html"
