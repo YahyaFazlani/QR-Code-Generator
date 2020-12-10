ce0edeebc7c021a069121ba0d991b9ed90351350
Yahya Fazlani <yahyafazlani@outlook.com>
2020-12-10 13:15:08 +0530

    change

--- Pages/urls.py
+++ Pages/urls.py
@@ -4,5 +4,5 @@ from .views import index, google_file
 
 urlpatterns = [
   path('', index),
-  path('google5c02155bdaa39045.html/', google_file)
+  path('google5c02155bdaa39045.html', google_file)
 ]
